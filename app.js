6cc70551864403acf02477433242b5e619c48c88
Petya Angelova <bedfordpetya@gmail.com>
2022-04-30 19:47:27 +0300

    final

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
+    const startBtn = document.querySelector('.beer-button')
+    const randomBeer = document.querySelector('.random-beer')
+    const descriptionDisplay = document.querySelector('.description')
+    const beerImg = document.querySelector('.beer-img')
 
-    fetch('https://api.punkapi.com/v2/beers/random')
-    .then(response =>{
-        return response.json()
-    })
-    .then(data => {
-        //console.log(data);
-        const name = data[0].name
-        const description = data[0].description
-        const image = data[0].image_url
-        const unit = data[0].volume.unit
-        const value = data[0].volume.value
-        //console.log(image);
-      
-    })
+    function getData() {
+
+        fetch('https://api.punkapi.com/v2/beers/random')
+            .then(response => response.json())
+            .then(data => {
+                //console.log(data);
+                const name = data[0].name
+                const description = data[0].description
+                const image = data[0].image_url
+                const unit = data[0].volume.unit
+                const value = data[0].volume.value
+                //console.log(image);
+
+                randomBeer.innerHTML = name + ' ' + value + ' ' + unit
+                descriptionDisplay.innerHTML = description
+                beerImg.src = image
+            })
+            .catch(err => {
+        console.log(`error ${err}`)
+        })
+    }
+    startBtn.addEventListener('click', getData)
 })
